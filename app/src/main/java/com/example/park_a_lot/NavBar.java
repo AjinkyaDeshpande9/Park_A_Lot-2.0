dcade17013c3527961cb376c06d947899349ceca
Ajinkya_Deshpande <deshpande.ajinkya09@gmail.com>
2022-04-26 17:12:57 +0530

    Logout (Menu option) and Mail (via Floater) Removed Logout from nav bar.

--- app/src/main/java/com/example/park_a_lot/NavBar.java
+++ app/src/main/java/com/example/park_a_lot/NavBar.java
@@ -1,7 +1,11 @@
 package com.example.park_a_lot;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -14,15 +18,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.park_a_lot.databinding.ActivityNavBarBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NavBar extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityNavBarBinding binding;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         binding = ActivityNavBarBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -31,8 +38,10 @@ public class NavBar extends AppCompatActivity {
         binding.appBarNavBar.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent mailIntent = new Intent(Intent.ACTION_VIEW);
+                Uri data = Uri.parse("mailto:?subject=" + "I have Issues/ Suggestions"+ "&body=" + "**Thank you for contacting Park A Lot.\n Please reference your Mobile Number while writing your query.\n We will get back to you shortly! " + "&to=" + "parkalot4@gmail.com");
+                mailIntent.setData(data);
+                startActivity(Intent.createChooser(mailIntent, "Contact Us"));
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -53,6 +62,14 @@ public class NavBar extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.nav_bar, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+        return  true;
     }
 
     @Override
