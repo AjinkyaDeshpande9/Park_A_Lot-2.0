f7b3ac134a54b0f9e9e7144dea8888497d578a98
Ajinkya_Deshpande <deshpande.ajinkya09@gmail.com>
2022-04-27 10:12:47 +0530

    Booking icon in Home Fragment, Booking Page Created and Parking Lots xml created.

---
+++ app/src/main/java/com/example/park_a_lot/BookingPage.java
@@ -0,0 +1,15 @@
+package com.example.park_a_lot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_page);
+    }
+
+}
--- app/src/main/java/com/example/park_a_lot/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/park_a_lot/ui/home/HomeFragment.java
@@ -1,15 +1,19 @@
 package com.example.park_a_lot.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.park_a_lot.BookingPage;
+import com.example.park_a_lot.R;
 import com.example.park_a_lot.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -18,15 +22,25 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+//        HomeViewModel homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
+//
+//        binding = FragmentHomeBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final TextView textView = binding.button;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
+        View view = inflater.inflate(R.layout.fragment_home,container ,false);
+        Button btnOpen = (Button) view.findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), BookingPage.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 
     @Override
