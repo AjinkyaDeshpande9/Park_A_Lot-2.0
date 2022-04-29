37b7ba7ce89e198c6d418507ed04d3d3cbda4e8b
Ajinkya_Deshpande <deshpande.ajinkya09@gmail.com>
2022-04-29 19:11:44 +0530

    Basic Functionality issue Solved

--- app/src/main/java/com/example/park_a_lot/BookingSummary.java
+++ app/src/main/java/com/example/park_a_lot/BookingSummary.java
@@ -9,20 +9,11 @@ import android.widget.Button;
 
 public class BookingSummary extends AppCompatActivity {
 
-    Button ConfirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_summary);
-        ConfirmButton = findViewById(R.id.ConfirmtoBook);
-
-        ConfirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), ConfirmedBooking.class));
-            }
-        });
     }
 
 
