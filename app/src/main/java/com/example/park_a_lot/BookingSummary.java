8989da4cd6128b306748f41f665ff0ebb8b149d5
Ajinkya_Deshpande <deshpande.ajinkya09@gmail.com>
2022-04-29 19:00:19 +0530

    Basic Functionality issue Solved

--- app/src/main/java/com/example/park_a_lot/BookingSummary.java
+++ app/src/main/java/com/example/park_a_lot/BookingSummary.java
@@ -2,15 +2,29 @@ package com.example.park_a_lot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class BookingSummary extends AppCompatActivity {
 
+    Button ConfirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_summary);
+        ConfirmButton = findViewById(R.id.ConfirmtoBook);
+
+        ConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ConfirmedBooking.class));
+            }
+        });
     }
+
+
+
 }
--- app/src/main/java/com/example/park_a_lot/ConfirmedBooking.java
+++ app/src/main/java/com/example/park_a_lot/ConfirmedBooking.java
@@ -2,13 +2,28 @@ package com.example.park_a_lot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.park_a_lot.ui.home.HomeFragment;
 
 public class ConfirmedBooking extends AppCompatActivity {
+
+    Button BackToHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmed_booking);
+        BackToHome = findViewById(R.id.BackToHome);
+
+        BackToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), HomeFragment.class));
+            }
+        });
     }
 }
